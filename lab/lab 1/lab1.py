6ad3fde796405cd7cd37115306e6e09798afd5fa
likith <172051106+agent7898@users.noreply.github.com>
2026-06-17 21:00:44 +0530

    Added gitignore now

--- lab/lab 1/lab1.py
+++ lab/lab 1/lab1.py
@@ -1,24 +1,19 @@
 import matplotlib
-matplotlib.use('Qt5Agg')  # Use TkAgg backend
+matplotlib.use('Qt5Agg')  
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 
-# Load the California Housing Dataset
 data = fetch_california_housing(as_frame=True)
-
 df = data.frame
 print(df)
-# Select numeric columns
-columns = df.select_dtypes(include=['float64', 'int64']).columns
-len(columns)
-print (len(columns))
-# Set up grid layout (two rows: one for histograms, one for box plots)
-n_columns = 3
-n_rows = (len(columns) + n_columns - 1) // n_columns  # Ensure enough rows
 
-fig, axes = plt.subplots(n_rows , n_columns, figsize=(15,30))  # Double rows
+columns = df.select_dtypes(include=['float64', 'int64']).columns
+
+n_columns = 3
+n_rows = 3
+
+fig, axes = plt.subplots(n_rows , n_columns, figsize=(15,30)) 
 axes = axes.flatten()
-# Plot histograms
 for i, column in enumerate(columns):
     ax = axes[i]
     df[column].hist(bins=30, edgecolor='black', grid=False, ax=ax)
@@ -27,14 +22,13 @@ plt.tight_layout()
 plt.savefig("Histogram_plots.png")
 plt.show()
 
-# Plot Boxplot
-fig, axes = plt.subplots(n_rows , n_columns, figsize=(15,10))  # Double rows
+
+fig, axes = plt.subplots(n_rows , n_columns, figsize=(15,10))  
 axes = axes.flatten()
 for i, column in enumerate(columns):
-    ax = axes[i]  # Now, correctly indexing box plots
+    ax = axes[i]  
     df.boxplot(column=column, grid=False, ax=ax)
     ax.set(title=f"Box Plot of {column}")
-# Adjust layout and display
 plt.tight_layout()
 plt.savefig("BOX_plots.png")
 plt.show()
